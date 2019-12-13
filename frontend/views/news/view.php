328c84a812c4cd5fbc64962a4d4e3042d93be39f
jake47 <zhandos38@gmail.com>
2019-12-13 21:54:23 +0600

    Posts images fixes

--- frontend/views/news/view.php
+++ frontend/views/news/view.php
@@ -15,6 +15,9 @@ $imgPath = Yii::$app->params['staticDomain'] . '/posts/';
     </strong>
     <br>
 </h2>
+<div class="news__content-view">
+    <?= $model->content ?>
+</div>
 <div class="news__comment">
     <h2>Комментарии</h2>
     <p><em>*Оставлять комментарии могут только зарегистрированные пользователи.&nbsp;</em><a href="https://rnoik.ru/personal/?register=yes"><em>Зарегистрируйтесь</em></a><em>&nbsp;или&nbsp;</em><a href="https://rnoik.ru/personal/"><em>авторизируйтесь</em></a><br></p>
