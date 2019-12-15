c9c12bc5ffe2ca3bb265cb206ea5c490a8433538
jake47 <zhandos38@gmail.com>
2019-12-15 17:46:23 +0600

    Expert opinion fixes

--- frontend/views/science-and-practice/expert-opinion.php
+++ frontend/views/science-and-practice/expert-opinion.php
@@ -8,16 +8,16 @@ $this->params['breadcrumbs'][] = ['label' => 'Наука и практика'];
 $this->params['breadcrumbs'][] = ['label' => 'Рекомендаций', 'url' => ['/science-and-practice/expert-opinions']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<h2 class="recommendation__title">
+<h2 class="v-expert-opinion__title">
     <strong>
         <?= $model->title ?>
     </strong>
     <br>
 </h2>
-<div class="recommendation__content-view">
+<div class="v-expert-opinion__content-view">
     <?= $model->content ?>
 </div>
-<div class="recommendation__comment">
+<div class="v-expert-opinion__comment">
     <h2>Комментарии</h2>
     <p><em>*Оставлять комментарии могут только зарегистрированные пользователи.&nbsp;</em><a href="https://rnoik.ru/personal/?register=yes"><em>Зарегистрируйтесь</em></a><em>&nbsp;или&nbsp;</em><a href="https://rnoik.ru/personal/"><em>авторизируйтесь</em></a><br></p>
 </div>
