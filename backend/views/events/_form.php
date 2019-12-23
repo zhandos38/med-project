6d9fff40d7b0b99a02873edd3de0476f7ea09162
jake47 <zhandos38@gmail.com>
2019-12-23 21:36:35 +0600

    Events editor fixes

--- backend/views/events/_form.php
+++ backend/views/events/_form.php
@@ -35,7 +35,7 @@ use mihaildev\elfinder\ElFinder;
     ]); ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
