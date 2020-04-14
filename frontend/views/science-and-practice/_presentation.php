8b79ebbf8eb96760662ab4b7e7e3a5369ebabc07
jake47 <zhandos38@gmail.com>
2020-04-14 13:21:43 +0600

    Presentation fixes

--- frontend/views/science-and-practice/_presentation.php
+++ frontend/views/science-and-practice/_presentation.php
@@ -11,13 +11,12 @@ $imgPath = Yii::$app->params['staticDomain'] . '/presentations/';
         <div class="presentation__author">
             Автор: <?= $model->author ?>
         </div>
+        <?php if (!empty($model->year)): ?>
         <div class="presentation__footer">
             <div class="presentation__year">
                 <i class="presentation__year-icon fa fa-calendar"></i> <?= $model->year ?>
             </div>
-            <div class="presentation__views">
-                <i class="presentation__views-icon fa fa-eye"></i> <?= $model->views ?>
-            </div>
         </div>
+        <?php endif; ?>
     </div>
 </a>
